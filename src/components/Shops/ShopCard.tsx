5d564cc2a12d1830f61fb183b8fb1dd812157b57
tigawanna <denniskinuthiaw@gmail.com>
2022-06-25 07:36:19 +0300

    added add shop tile

--- src/components/Shops/ShopCard.tsx
+++ src/components/Shops/ShopCard.tsx
@@ -1,42 +1,71 @@
-import React from 'react'
-import { Shop } from '../../utils/other/types';
+import React from "react";
+import { FaPlus } from "react-icons/fa";
+import { Shop } from "../../utils/other/types";
+import { IconContext } from 'react-icons';
 
 interface ShopCardProps {
-shop:Shop
+  shop: Shop;
 }
 
-export const ShopCard: React.FC<ShopCardProps> = ({shop}) => {
-return (
- <div className=' p-5 m-1 flex-col bg-slate-500 shadow-md shadow-purple-900 
- hover:shadow-lg rounded w-[95%] md:w-[30%] '>
-<div className='text-xl font-bold'>{shop.shopnumber}</div>
-<div className='h-[80%] flex-col justify-end items-end'>
-<div className='text-lg'>{shop.shopfloor}</div>
-<div className='text-6xl'>{shop.shopname}</div>
-<div className='text-lg font-medium'>{shop.monthlyrent}</div>
-</div>
- </div>
-);
-}
-
+export const ShopCard: React.FC<ShopCardProps> = ({ shop }) => {
+  return (
+    <div
+      className=" p-5 m-1 flex-col bg-slate-500 shadow-md shadow-purple-900 
+                           hover:shadow-lg rounded w-[95%] md:w-[30%] ">
+      <div className="text-xl font-bold">{shop.shopnumber}</div>
+      <div className="h-[80%] flex-col justify-end items-end">
+        <div className="text-lg">{shop.shopfloor}</div>
+        <div className="text-6xl capitalize">{shop.shopname}</div>
+        <div className="text-lg font-medium">{shop.monthlyrent}</div>
+      </div>
+    </div>
+  );
+};
 
 
 
 
 interface ShopFloorProps {
-floor:string
-setFloor: React.Dispatch<React.SetStateAction<string>>
+  floor: string;
+  setFloor: React.Dispatch<React.SetStateAction<string>>;
+  current: string;
 }
 
+export const ShopFloor: React.FC<ShopFloorProps> = ({
+  floor,
+  setFloor,
+  current,
+}) => {
+  const floorClicked = (floor: string) => {
+    setFloor(floor);
+  };
+  const selected = current === floor ? { background: "purple" } : {};
+  return (
+    <div
+      className="w-24 h-fit p-2 m-1 bg-slate-500 hover:bg-slate-600 text-lg font-normal 
+ text-center capitalize cursor-pointer"
+      onClick={() => floorClicked(floor)}
+      style={selected}
+    >
+      {floor}
+    </div>
+  );
+};
 
-export const ShopFloor: React.FC<ShopFloorProps> = ({floor,setFloor}) => {
-const floorClicked=(floor:string)=>{setFloor(floor)} 
-return (
- <div 
- className='w-fit h-full p-2 m-1 bg-slate-500 text-lg font-normal text-center capitalize cursor-pointer'
- onClick={()=>floorClicked(floor)}
- >
-{floor}
- </div>
-);
-}
+
+
+interface AddShopCardProps {
+ 
+  }
+  
+  export const AddShopCard: React.FC<AddShopCardProps> = ({ }) => {
+    return (
+      <div className=" p-5 m-1 flex-center bg-slate-500 shadow-md shadow-purple-900 
+      hover:shadow-lg rounded w-[95%] md:w-[30%] ">
+         <IconContext.Provider value={{ size: "45px", className: "table-edit-icons" }} >
+         <FaPlus/>
+         </IconContext.Provider>
+    
+      </div>
+    );
+  };
--- src/components/Shops/Shops.tsx
+++ src/components/Shops/Shops.tsx
@@ -1,61 +1,64 @@
-import { User } from 'firebase/auth';
-import { db } from '../../firebase/firebaseConfig';
+import { User } from "firebase/auth";
+import { db } from "../../firebase/firebaseConfig";
 import { collection, query, orderBy, where } from "firebase/firestore";
-import { useFirestoreQueryData } from '@react-query-firebase/firestore';
-import { useState } from 'react';
-import { Shop } from '../../utils/other/types';
-import { ShopCard, ShopFloor } from './ShopCard';
-
+import { useFirestoreQueryData } from "@react-query-firebase/firestore";
+import { useState } from "react";
+import { Shop } from "../../utils/other/types";
+import { AddShopCard, ShopCard, ShopFloor } from "./ShopCard";
 
 interface ShopsProps {
-user?:User|null
+  user?: User | null;
 }
 
-export const Shops: React.FC<ShopsProps> = ({user}) => {
+export const Shops: React.FC<ShopsProps> = ({ user }) => {
+  const [floor, setFloor] = useState("first");
+  const shopsRef = query(
+    collection(db, "shops", floor, "shops"),
+    orderBy("shopnumber", "asc"),
+    where("shopfloor", "==", floor)
+  );
+  const floors = ["ground", "first", "second", "third"];
 
-const [floor, setFloor] = useState("first")
-const shopsRef = query( collection(db,"shops",floor,"shops"),orderBy("shopnumber","asc"),
-   where("shopfloor", "==", floor)
-);
-const floors=["ground","first","second","third"]
+  const shopQuery = useFirestoreQueryData(["shops", floor], shopsRef);
+  // console.log("query  ==== ",shopQuery)
+  // console.log("shop query ==== ",shopQuery?.data)
+  const data = shopQuery.data as Shop[];
+  if (shopQuery.error) {
+    return (
+      <div className="w-full h-full flex-center">
+        ERROR LOADING SHOPS {shopQuery.error.message}
+      </div>
+    );
+  }
 
- 
+  if (shopQuery.isLoading) {
+    return <div className="w-full h-full flex-center"> loading ..... </div>;
+  }
 
-const shopQuery = useFirestoreQueryData(["shops",floor],shopsRef);
-// console.log("query  ==== ",shopQuery)
-// console.log("shop query ==== ",shopQuery?.data)
-const data = shopQuery.data as Shop[]
-if(shopQuery.error){
-return (
-<div className='w-full h-full flex-center'> ERROR LOADING SHOPS {shopQuery.error.message}  </div>);
-}
+  return (
+    <div className="w-full h-full flex-col">
+      <div className="right-0 left-0 flex flex-wrap h-[10%]w-full ">
 
-if(shopQuery.isLoading){
-return (
-<div className='w-full h-full flex-center'> loading .....  </div>);
-}
+        <div className="fixed flex h-[10%] w-full mx-1 justify-center">
+          {floors.map((afloor, index) => (
+            <ShopFloor
+              floor={afloor}
+              setFloor={setFloor}
+              key={index}
+              current={floor}
+            />
+          ))}
+        </div>
+        
+      </div>
 
-
-return (
-<div className='w-full h-full flex-col'>
-<div className='flex flex-wrap h-[10%]w-full '>
-
-<div className='flex h-[10%] w-full mx-1'>
-    {floors.map((floor,index)=>{
-        return(<ShopFloor floor={floor} setFloor={setFloor} key={index}/>)
-    })}
-</div>
-</div>
-
-<div className='flex flex-wrap  w-full bg-pink-300 justify-center m-2'>
-{
-data.map((shop,index)=>{
-return(<ShopCard shop={shop} key={index}/>)
-})
-
-}
-</div>
-</div>
-
-);
-}
+      <div className="flex flex-wrap  w-full justify-center m-2 mt-14">
+      <AddShopCard/>
+        {data.map((shop, index) => {
+          return <ShopCard shop={shop} key={index} />;
+        })}
+  
+      </div>
+    </div>
+  );
+};
